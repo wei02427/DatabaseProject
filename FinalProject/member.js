f3f4a990fbb59e8e3daccd6f7c7bc22fe5d3e770
w190225 <wei.141227@gmail.com>
2019-12-23 14:24:48 +0800

    會員子系統

---
+++ FinalProject/member.js
@@ -0,0 +1,38 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    port: '3306',
+    database: 'project'
+});
+
+pool.getConnection(function (err, connection) {
+    if (err) throw err;
+    console.log(register('q', 'p', 'q', '1111', '123', '0', '123456', 'ded', '1987-1-3', '123455'));
+    connection.query(register('q', 'p', 'q', '1111', '123', '0', '123456', 'ded', '1987-1-3', '123455'), function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    })
+    connection.query('SELECT * FROM `Member`', function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        connection.release();
+    })
+
+});
+
+
+function register(fname, lname, email, phone, address, sex, password, account, birthday, credits) {
+    return "INSERT INTO `Member` SET `First_Name`='" + fname +
+        "',`Last_Name`='" + lname +
+        "',`Sex`=" + sex +
+        ", `Email`='" + email +
+        "',`Phone`='" + phone +
+        "',`Password`='" + password +
+        "',`Credits`='" + credits +
+        "',`Class`='1'" +
+        ",`Birthday`='" + birthday +
+        "',`Address`='" + address +
+        "',`Account`='" + account + '\''
+}
