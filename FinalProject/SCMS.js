c535137c563f40185755a144ba0dc63be24ef47c
kukuman98 <khaiyi5893@gmail.com>
2019-12-23 20:40:37 +0800

    debug QQ 製作變數function 還有更改pool的應用

--- FinalProject/SCMS.js
+++ FinalProject/SCMS.js
@@ -7,75 +7,51 @@ var pool = mysql.createPool({
     port: '3306',
     database: 'project'
 });
- 
 
 
-
-
-pool.getConnection(function(err, connection) {
-    var  sql = commentGame(1,1);
-  // Use the connection
-    if (err) throw err;
-    connection.query(sql, function(err, result) {
-        if(err)throw err;
-        var string=JSON.stringify(result.length); 
-        console.log(sql);
-        
-        // Don't use the connection here, it has been returned to the pool.
+//insert comment to mysql
+var commentGame = function(ID,Game_ID,comment,star){     
+    var dateTime = getRealTime();
+    var sqlCommand = "INSERT into `comment` (`ID`,`Game_ID`,`description`,`stars`,`time`)VALUES (?,?,?,?,?)";
+    const inserts = [ID,Game_ID,comment,star,dateTime]
+    sqlCommand = mysql.format(sqlCommand, inserts)
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('error connecting: ', err);
+        }
+        else{
+            connection.query(sqlCommand, function(err, result) {
+                if(err){
+                    connection.rollback(function () {
+                        connection.release();
+                        //Failure
+                    });
+                }
+                else{
+                    var string=JSON.stringify(result.length); 
+                    console.log(sqlCommand)
+                    connection.release();
+                }
+            });    
+        }
     });
+}
+
+commentGame(1,1,"testing",5);
+
+function getCommentAccount(){
+    var sqlCommand = "SELECT "
+
+}
 
 
-
-    //var  sql = '';
-
-
-    /*function getEvent() {
-        var input = document.getElementById("SCMSGetEvent").string;
-        //alert(input);
-    }*/
-
-    //insert comment to mysql
-    function commentGame(ID,Game_ID){
-        //var comment = document.getElementById("SCMSGetComment").string;
-        //var star = document.getElementById("SCMSGetStar").string;
-        var comment = "testing";
-        var star = 1;
-        var sqlCommand;
-        var commentID;
-        var dateTime = getRealTime(); //call function get real time 
-        //get comment amount to calculate commentID
-        connection.query("SELECT * FROM comment", function(err, result) {
-            if(err)throw err;
-            commentID=JSON.stringify(result.length); 
-            console.log(commentID);
-            // Don't use the connection here, it has been returned to the pool.
-        });
-
-        
-        sqlCommand = "INSERT into `comment` (`ID`,`Game_ID`,`description`,`stars`,`time`)VALUES (?,?,?,?,?)";
-        const inserts = [ID,Game_ID,comment,star,dateTime]
-        sqlCommand = mysql.format(sqlCommand, inserts)
-        return sqlCommand;
-    }
-
-
-    function getCommentAccount(){
-
-
-    }
-
-
-    function getAllGameComment(){
-
-    }
-    function getRealTime(){
-        var today = new Date();
-        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var dateTime = date+' '+time;
-        return dateTime;
-    }
-
-
-    connection.release();
-});
+function getGameComment(Game_ID){
+    var sqlCommand = "SELECT *"
+}
+function getRealTime(){
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    return dateTime;
+}
