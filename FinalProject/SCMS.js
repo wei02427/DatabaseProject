be27d600101a6270f5bf303433c355f1b9ada47e
kukuman98 <khaiyi5893@gmail.com>
2019-12-24 14:59:24 +0800

    SCMS.js.0 finish
    用法——
    var 變數 = getAllComment(ID,Game_ID);
    可以把使用者的個人評論放在第一個變數裏面-變數[0]。
    
    commentGame（ID,Game_ID,description,stars);
    可以判斷使用者是否已經評論過此游戲，若有則修改mysql的資料，若無則新增一個評論在mysql裏面

--- FinalProject/SCMS.js
+++ FinalProject/SCMS.js
@@ -38,7 +38,7 @@ async function getPersonComment(ID, Game_ID){
     sqlCommand = mysql.format(sqlCommand, inserts);
     try {
         const results = await query(sqlCommand);
-        console.log("success");
+        console.log("success get person comment");
         return results;
     }
     catch{
@@ -75,6 +75,20 @@ async function getCommentID(Game_ID){
     }
 }
 
+async function deleteGameComment(Game_ID){
+    var sqlCommand = "SELECT `ID` FROM comment WHERE `Game_ID` = ?";
+    const inserts = [Game_ID];
+    sqlCommand = mysql.format(sqlCommand, inserts);
+    try {
+        const results = await query(sqlCommand);
+        console.log("success get comment id");
+        return results
+    }
+    catch{
+        console.log(err);
+    }
+}
+
 function compareObj(obj1,obj2){
     var i=0;
     while(obj2[i]){
@@ -84,6 +98,37 @@ function compareObj(obj1,obj2){
         i++; 
     }
     return false;
+}
+
+function getAllComment(ID,Game_ID){
+    var object1 = getPersonComment(ID,Game_ID);
+    var object2 = getGameComment(ID,Game_ID);
+    var comment = [];
+    object1
+    .then(function(results){
+        var i = 0;
+        while(results[i]){
+            comment.push(results[i]);
+            i++;
+        }
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+    object2
+    .then(function(results){
+        var i = 0;
+        while(results[i]){
+            comment.push(results[i]);
+            i++;
+        }
+        console.log(comment);
+        return comment;
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+
 }
 
 function commentGame(ID,Game_ID,des,star){
@@ -115,13 +160,5 @@ function getRealTime(){
     return dateTime;
 }
 
-commentGame(1,1,"shit",1);
-
-/*var text = getGameComment(2,1);
-text
-.then(function(results){
-    console.log(results);
-})
-.catch(function(err){
-    console.log(err);
-});*/
+//commentGame(2,1,"wtf lah",5);
+var commentObj = getAllComment(2,1);
